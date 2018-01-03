80811f6b61124da77ad305949f38cfab28de19dd
jwhite <joshua.white@twintechs.com>
2018-01-03 12:52:07 -0700

    new

--- html/test2.php
+++ html/test2.php
@@ -11,7 +11,7 @@
 
 <?php
 
-   if( $_POST["service"] == "service1" || $_POST["service"] == "service2" ) {
+   if( $_POST["service"] == "service1" || $_POST["service"] == "services" ) {
 
       echo  $_POST['service']. " is valid service".  "<br />";
 
@@ -19,7 +19,8 @@
       $dbuser = 'root';
       $dbpass = 'connectgfe1!';
       $dbname = $_POST["service"];
-      $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+//      $dbname = 'service1';
+      $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
    
       if(! $conn ) {
         die('could not connect to mysql: ' . mysql_error());
@@ -29,21 +30,48 @@
 
 // do stuff with databse
 
-      $sql = 'select name';
-      $retval = $conn->query($sql);
-   
-      if(! $retval ) {
-        die('Could not get data: ' . mysqli_error());
-      }
+         $sql = "show tables";
+
+//       $sql = "select name from serv1";
+//       $sql = "select table_name from information_schema.tables where table_schema='service1'";
+ 
+        $retval = $conn->query($sql);
+
+
+// note how calls are made to retval, below operate the same
+
+// 1
 
 /*
-      $row= $retval->fetch_assoc();
-      echo "name: ".$row["name"].  "<br> ";
-*/
-      while($row = mysqli_fetch_assoc($result)) {
-        echo "name " . $row["name"]. "<br>";
-      }
+        $row2 = $retval->fetch_array(MYSQLI_NUM);
+        printf("%s \n", $row2[0]);
 
+        $row2 = $retval->fetch_array(MYSQLI_NUM);
+        printf("%s \n", $row2[0]);
+*/
+
+
+// 2 
+        
+      if (mysqli_num_rows($retval) > 0) {
+        // output data of each row
+
+/*
+        // for use in table with assoc data
+        while($row = mysqli_fetch_array($retval,MYSQLI_ASSOC)) {
+          echo "name: " . $row["name"].  "<br>";
+        }
+*/
+
+
+        // for use in any query
+        while($row = $retval->fetch_array(MYSQLI_NUM)) {
+           echo $row[0]. "<br>";
+        }
+ 
+      } 
+
+////
 
 
 // close database
